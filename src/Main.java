8641fa57023f31df352d51255505f8f1f98c3e48
ndaleksey <ugtemp@mail.ru>
2022-07-30 12:46:25 +0300

    Apply reading from file

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
 import org.w3c.dom.Document;
 import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.*;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class Main {
     private static ArrayList<Marks> marks = new ArrayList<>();
     private static boolean isFound;
+    private final static String FILE_NAME = "/home/nd/Projects/Java/Java_XMLParser/cars.xmls";
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+
+        if (!Files.exists(Path.of(FILE_NAME))) {
+            System.out.printf("Error reading file: file (%s) not exist\n", FILE_NAME);
+            return;
+        }
 
         String url = "https://www.drom.ru/cached_files/autoload/files/ref.xml";
         DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
@@ -21,16 +32,16 @@ public class Main {
         DocumentBuilder b = f.newDocumentBuilder();
         URLConnection urlConnection = new URL(url).openConnection();
         urlConnection.addRequestProperty("Accept", "application/xml");
-        Document doc = b.parse(urlConnection.getInputStream());
-        doc.getDocumentElement().normalize();
-        System.out.println ("Root element: " +  doc.getDocumentElement().getNodeName());
-        System.out.println("66:");
-
 
         SAXParserFactory factory = SAXParserFactory.newInstance();
         SearchingXMLHandler scHandler = new SearchingXMLHandler("Marks");
-        SAXParser prs = factory.newSAXParser();
-        prs.parse(url, scHandler);
+
+        XMLReader reader = factory.newSAXParser().getXMLReader();
+        reader.setContentHandler(scHandler);
+
+        InputSource source = new InputSource(FILE_NAME);
+        source.setEncoding(StandardCharsets.UTF_8.displayName());
+        reader.parse(source);
 
         for (Marks mark : marks) {
             System.out.println(String.format("%s %s ", mark.getIdMark(), mark.getsMark()) );
