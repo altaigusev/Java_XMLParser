241e06b1d175de6a64db2b00902dbdbb8475e541
sshunpik <altaigusev@gmail.com>
2022-07-30 13:33:37 +0700

    XML parsing drom.ru

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ import javax.xml.parsers.*;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class Main {
@@ -76,70 +75,5 @@ public class Main {
                 isEntered = false;
         }
     }
-//    private static class XMLHandler extends DefaultHandler {
-//        @Override
-//        public void startDocument() throws SAXException {
-//
-//        }
-//
-//        @Override
-//        public void endDocument() throws SAXException {
-//
-//        }
-//
-//        @Override
-//        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-//            if (qName.equals("employee")) {
-//                String name = attributes.getValue("name");
-//                String job = attributes.getValue("job");
-//                employees.add(new Employee(name, job));
-//            }
-//        }
-//
-//        @Override
-//        public void endElement(String uri, String localName, String qName) throws SAXException {
-//
-//        }
-//
-//        @Override
-//        public void characters(char[] ch, int start, int length) throws SAXException {
-//
-//        }
-//
-//        @Override
-//        public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
-//
-//        }
-//    }
-//
-//    private static class AdvancedXMLHandler extends DefaultHandler {
-//        private String name, job, lastElementName;
-//
-//        @Override
-//        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-//            lastElementName = qName;
-//        }
-//
-//        @Override
-//        public void characters(char[] ch, int start, int length) throws SAXException {
-//            String information = new String(ch, start, length);
-//            information = information.replace("\n", "").trim();
-//            if (!information.isEmpty()) {
-//                if (lastElementName.equals("name"))
-//                    name = information;
-//                if (lastElementName.equals("job"))
-//                    job = information;
-//            }
-//        }
-//
-//        @Override
-//        public void endElement(String uri, String localName, String qName) throws SAXException {
-//            if ( (name != null && !name.isEmpty()) && (job != null && !job.isEmpty()) ) {
-//                employees.add(new Employee(name, job));
-//                name = null;
-//                job = null;
-//            }
-//        }
-//    }
 }
 
