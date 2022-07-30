a7e7defb026e7087d175761e3ed4dc47d0b5948d
sshunpik <altaigusev@gmail.com>
2022-07-30 13:32:08 +0700

    XML parsing drom.ru

--- src/Main.java
+++ src/Main.java
@@ -1,94 +1,145 @@
+import org.w3c.dom.Document;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import java.io.File;
+import javax.xml.parsers.*;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
-
 public class Main {
-    private static ArrayList<Employee> employees = new ArrayList<>();
+    private static ArrayList<Marks> marks = new ArrayList<>();
+    private static boolean isFound;
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
 
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        String url = "https://www.drom.ru/cached_files/autoload/files/ref.xml";
+        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+        f.setNamespaceAware(false);
+        f.setValidating(false);
+        DocumentBuilder b = f.newDocumentBuilder();
+        URLConnection urlConnection = new URL(url).openConnection();
+        urlConnection.addRequestProperty("Accept", "application/xml");
+        Document doc = b.parse(urlConnection.getInputStream());
+        doc.getDocumentElement().normalize();
+        System.out.println ("Root element: " +  doc.getDocumentElement().getNodeName());
+        System.out.println("66:");
+
+
         SAXParserFactory factory = SAXParserFactory.newInstance();
-        SAXParser parser = factory.newSAXParser();
+        SearchingXMLHandler scHandler = new SearchingXMLHandler("Marks");
+        SAXParser prs = factory.newSAXParser();
+        prs.parse(url, scHandler);
 
-//        XMLHandler handler = new XMLHandler();
-//        parser.parse(new File("src/tst.xml"), handler);
-        AdvancedXMLHandler handler = new AdvancedXMLHandler();
-        parser.parse(new File("src/tst2.xml"), handler);
-        for (Employee employee : employees)
-        System.out.println(String.format("Имя сотрудника: %s, его должность: %s", employee.getName(), employee.getJob()));
+        for (Marks mark : marks) {
+            System.out.println(String.format("%s %s ", mark.getIdMark(), mark.getsMark()) );
+        }
     }
 
-    private static class XMLHandler extends DefaultHandler {
-        @Override
-        public void startDocument() throws SAXException {
 
-        }
+    private static class SearchingXMLHandler extends DefaultHandler {
+        private String element;
+        private String idMark, sMark;
+        private boolean isEntered;
 
-        @Override
-        public void endDocument() throws SAXException {
-
+        public SearchingXMLHandler(String element) {
+            this.element = element;
         }
 
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            if (qName.equals("employee")) {
-                String name = attributes.getValue("name");
-                String job = attributes.getValue("job");
-                employees.add(new Employee(name, job));
+            element = qName;
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            String info = new String(ch, start, length);
+//            if (Character.UnicodeBlock.of(info.charAt(0)).equals(Character.UnicodeBlock.CYRILLIC)) info = "Cyrillic carname";
+            if (Character.UnicodeBlock.of(info.charAt(0)).equals(Character.UnicodeBlock.CYRILLIC)) System.out.println("info = " + info);
+            if(!info.isEmpty()) {
+                if (element.equals("idMark")) idMark = info;
+                if (element.equals("sMark")) sMark = info;
             }
         }
 
         @Override
         public void endElement(String uri, String localName, String qName) throws SAXException {
-
-        }
-
-        @Override
-        public void characters(char[] ch, int start, int length) throws SAXException {
-
-        }
-
-        @Override
-        public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
-
+            if( (idMark != null && !idMark.isEmpty()) && (sMark != null && !sMark.isEmpty())) {
+//                if (Character.UnicodeBlock.of(info.charAt(0)).equals(Character.UnicodeBlock.CYRILLIC)) System.out.println();
+                marks.add(new Marks(idMark, sMark));
+                idMark = null;
+                sMark = null;
+            }
+            if (qName.equals(element))
+                isEntered = false;
         }
     }
-
-    private static class AdvancedXMLHandler extends DefaultHandler {
-        private String name, job, lastElementName;
-
-        @Override
-        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            lastElementName = qName;
-        }
-
-        @Override
-        public void characters(char[] ch, int start, int length) throws SAXException {
-            String information = new String(ch, start, length);
-            information = information.replace("\n", "").trim();
-            if (!information.isEmpty()) {
-                if (lastElementName.equals("name"))
-                    name = information;
-                if (lastElementName.equals("job"))
-                    job = information;
-            }
-        }
-
-        @Override
-        public void endElement(String uri, String localName, String qName) throws SAXException {
-            if ( (name != null && !name.isEmpty()) && (job != null && !job.isEmpty()) ) {
-                employees.add(new Employee(name, job));
-                name = null;
-                job = null;
-            }
-        }
-    }
+//    private static class XMLHandler extends DefaultHandler {
+//        @Override
+//        public void startDocument() throws SAXException {
+//
+//        }
+//
+//        @Override
+//        public void endDocument() throws SAXException {
+//
+//        }
+//
+//        @Override
+//        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+//            if (qName.equals("employee")) {
+//                String name = attributes.getValue("name");
+//                String job = attributes.getValue("job");
+//                employees.add(new Employee(name, job));
+//            }
+//        }
+//
+//        @Override
+//        public void endElement(String uri, String localName, String qName) throws SAXException {
+//
+//        }
+//
+//        @Override
+//        public void characters(char[] ch, int start, int length) throws SAXException {
+//
+//        }
+//
+//        @Override
+//        public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+//
+//        }
+//    }
+//
+//    private static class AdvancedXMLHandler extends DefaultHandler {
+//        private String name, job, lastElementName;
+//
+//        @Override
+//        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+//            lastElementName = qName;
+//        }
+//
+//        @Override
+//        public void characters(char[] ch, int start, int length) throws SAXException {
+//            String information = new String(ch, start, length);
+//            information = information.replace("\n", "").trim();
+//            if (!information.isEmpty()) {
+//                if (lastElementName.equals("name"))
+//                    name = information;
+//                if (lastElementName.equals("job"))
+//                    job = information;
+//            }
+//        }
+//
+//        @Override
+//        public void endElement(String uri, String localName, String qName) throws SAXException {
+//            if ( (name != null && !name.isEmpty()) && (job != null && !job.isEmpty()) ) {
+//                employees.add(new Employee(name, job));
+//                name = null;
+//                job = null;
+//            }
+//        }
+//    }
 }
 
---
+++ src/Marks.java
@@ -0,0 +1,16 @@
+public class Marks {
+
+    private String idMark, sMark;
+    public Marks(String idMark, String sMark) {
+        this.idMark = idMark;
+        this.sMark = sMark;
+    }
+
+    public String getsMark() {
+        return sMark;
+    }
+
+    public String getIdMark() {
+        return idMark;
+    }
+}
